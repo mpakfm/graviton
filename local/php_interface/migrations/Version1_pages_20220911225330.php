975a700dffb447ee55037a19b592529ff726901f
Sergey Fomin <sfomin@synergy.ru>
2022-09-11 22:53:56 +0300

    migration page about

---
+++ local/php_interface/migrations/Version1_pages_20220911225330.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version1_pages_20220911225330 extends Version
+{
+    protected $description = "Страница \"О проекте\"";
+
+    protected $moduleVersion = "4.1.1";
+
+    /**
+     * @throws Exceptions\ExchangeException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+            ->IblockElementsImport()
+            ->setExchangeResource('iblock_elements.xml')
+            ->setLimit(20)
+            ->execute(function ($item) {
+                $this->getHelperManager()
+                    ->Iblock()
+                    ->addElement(
+                        $item['iblock_id'],
+                        $item['fields'],
+                        $item['properties']
+                    );
+            });
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
