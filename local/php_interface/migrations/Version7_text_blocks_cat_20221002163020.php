5d9e934e1b279b3f8bee6af0e958abc16acf7927
Sergey Fomin <sfomin@synergy.ru>
2022-10-02 16:31:20 +0300

    v7 миграции для инфоблока Текстовые блоки

---
+++ local/php_interface/migrations/Version7_text_blocks_cat_20221002163020.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version7_text_blocks_cat_20221002163020 extends Version
+{
+    protected $description = "";
+
+    protected $moduleVersion = "4.1.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+
+        $iblockId = $helper->Iblock()->getIblockIdIfExists(
+            'text',
+            'content'
+        );
+
+        $helper->Iblock()->addSectionsFromTree(
+            $iblockId,
+            array (
+  0 => 
+  array (
+    'NAME' => 'Главная',
+    'CODE' => '',
+    'SORT' => '500',
+    'ACTIVE' => 'Y',
+    'XML_ID' => NULL,
+    'DESCRIPTION' => '',
+    'DESCRIPTION_TYPE' => 'text',
+  ),
+)        );
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
