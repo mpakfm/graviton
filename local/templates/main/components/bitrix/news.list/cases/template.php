73b8e6056708046eb85a1b6a946d79a5aa467e3c
Bochkarev Ivan <ivanx86@gmail.com>
2022-10-17 00:27:52 +0600

    Update news

--- local/templates/main/components/bitrix/news.list/cases/template.php
+++ local/templates/main/components/bitrix/news.list/cases/template.php
@@ -18,44 +18,46 @@ $this->setFrameMode(true);
     <div class="l-default">
         <div class="l-content">
             <div class="news__container">
-                <div class="news-item">
-                    <?php foreach($arResult["ITEMS"] as $arItem) { ?>
-                        <?php
-                        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-                        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-                        ?>
-                    <div class="news-item__content" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-                        <div class="news-item__content--preview">
-                            <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-                                <div class="preview__img">
-                                    <picture>
-                                        <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt=""/>
-                                    </picture>
+                <div class="news-items">
+                    <div class="news-item">
+                        <?php foreach($arResult["ITEMS"] as $arItem) { ?>
+                            <?php
+                                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                            ?>
+                            <div class="news-item__content" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                                <div class="news-item__content--preview">
+                                    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+                                        <div class="preview__img">
+                                            <picture>
+                                                <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt=""/>
+                                            </picture>
+                                        </div>
+                                    <?endif?>
+                                    <a class="preview__more" href="<?echo $arItem["DETAIL_PAGE_URL"]?>">
+                                        <div class="preview__more--btn">Подробнее</div>
+                                        <div class="preview__more--arrow">
+                                            <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </div>
+                                    </a>
                                 </div>
-                            <?endif?>
-                            <a class="preview__more" href="<?echo $arItem["DETAIL_PAGE_URL"]?>">
-                                <div class="preview__more--btn">Подробнее</div>
-                                <div class="preview__more--arrow">
-                                    <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
+                                <div class="news-item__content--text">
+                                    <div class="text__title"><?echo $arItem["NAME"]?></div>
+                                    <div class="text__prefix"><?echo $arItem["PREVIEW_TEXT"]?></div>
+                                    <a class="text__more" href="<?echo $arItem["DETAIL_PAGE_URL"]?>">
+                                        <div class="text__more--btn">Подробнее</div>
+                                        <div class="text__more--arrow">
+                                            <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </div>
+                                    </a>
                                 </div>
-                            </a>
-                        </div>
-                        <div class="news-item__content--text">
-                            <div class="text__title"><?echo $arItem["NAME"]?></div>
-                            <div class="text__prefix"><?echo $arItem["PREVIEW_TEXT"]?></div>
-                            <a class="text__more" href="<?echo $arItem["DETAIL_PAGE_URL"]?>">
-                                <div class="text__more--btn">Подробнее</div>
-                                <div class="text__more--arrow">
-                                    <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                </div>
-                            </a>
-                        </div>
+                            </div>
+                        <?php } ?>
                     </div>
-                    <?php } ?>
                 </div>
             </div>
             <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
--- local/templates/main/components/mpakfm/search.page/common/template.php
+++ local/templates/main/components/mpakfm/search.page/common/template.php
@@ -74,83 +74,84 @@ if (is_array($arCloudParams["arrFILTER"])) {
             <?php } ?>
         </h2>
         <div class="news__container">
-            <?php if ($arResult["REQUEST"]["QUERY"] === false && $arResult["REQUEST"]["TAGS"] === false) { ?>
-            <?php } elseif ($arResult["ERROR_CODE"] != 0) { ?>
-                <p><?=GetMessage("SEARCH_ERROR")?></p>
-                <?ShowError($arResult["ERROR_TEXT"]);?>
-                <p><?=GetMessage("SEARCH_CORRECT_AND_CONTINUE")?></p>
-                <br /><br />
-                <p><?=GetMessage("SEARCH_SINTAX")?><br /><b><?=GetMessage("SEARCH_LOGIC")?></b></p>
-                <table border="0" cellpadding="5">
-                    <tr>
-                        <td align="center" valign="top"><?=GetMessage("SEARCH_OPERATOR")?></td><td valign="top"><?=GetMessage("SEARCH_SYNONIM")?></td>
-                        <td><?=GetMessage("SEARCH_DESCRIPTION")?></td>
-                    </tr>
-                    <tr>
-                        <td align="center" valign="top"><?=GetMessage("SEARCH_AND")?></td><td valign="top">and, &amp;, +</td>
-                        <td><?=GetMessage("SEARCH_AND_ALT")?></td>
-                    </tr>
-                    <tr>
-                        <td align="center" valign="top"><?=GetMessage("SEARCH_OR")?></td><td valign="top">or, |</td>
-                        <td><?=GetMessage("SEARCH_OR_ALT")?></td>
-                    </tr>
-                    <tr>
-                        <td align="center" valign="top"><?=GetMessage("SEARCH_NOT")?></td><td valign="top">not, ~</td>
-                        <td><?=GetMessage("SEARCH_NOT_ALT")?></td>
-                    </tr>
-                    <tr>
-                        <td align="center" valign="top">( )</td>
-                        <td valign="top">&nbsp;</td>
-                        <td><?=GetMessage("SEARCH_BRACKETS_ALT")?></td>
-                    </tr>
-                </table>
-            <?php } elseif (count($arResult["SEARCH"]) > 0) { ?>
-                <?php if ($arParams["DISPLAY_TOP_PAGER"] != "N") { ?>
-                    <?= $arResult["NAV_STRING"]; ?>
-                <?php } ?>
-                <div class="news-item">
-                    <?php foreach ($arResult["SEARCH"] as $arItem) { ?>
-                        <?php
-                            $dt = date_create_from_format('d.m.Y', $arItem['DATE_CHANGE']);
-                        ?>
-                    <div class="news-item__content">
-                        <div class="news-item__content--preview">
-                            <div class="preview__date"> <span class="preview__date--day"><?=$dt->format('d');?></span>
-                                <p class="preview__date--month"><?=FormatDate('F', $dt->format('U'));?></p>
-                            </div>
-                            <a class="preview__more" href="<?echo $arItem["URL"]?>">
-                                <div class="preview__more--btn">Подробее</div>
-                                <div class="preview__more--arrow">
-                                    <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="news-item__content--text">
-                            <div class="text__title"><?= $arItem["TITLE_FORMATED"]?></div>
-                            <div class="text__prefix"><?= $arItem["BODY_FORMATED"]?></div>
-                            <a class="text__more" href="<?echo $arItem["URL"]?>">
-                                <div class="text__more--btn">Подробее</div>
-                                <div class="text__more--arrow">
-                                    <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+            <div class="news-items">
+                <?php if ($arResult["REQUEST"]["QUERY"] === false && $arResult["REQUEST"]["TAGS"] === false) { ?>
+                <?php } elseif ($arResult["ERROR_CODE"] != 0) { ?>
+                    <p><?=GetMessage("SEARCH_ERROR")?></p>
+                    <?ShowError($arResult["ERROR_TEXT"]);?>
+                    <p><?=GetMessage("SEARCH_CORRECT_AND_CONTINUE")?></p>
+                    <br /><br />
+                    <p><?=GetMessage("SEARCH_SINTAX")?><br /><b><?=GetMessage("SEARCH_LOGIC")?></b></p>
+                    <table border="0" cellpadding="5">
+                        <tr>
+                            <td align="center" valign="top"><?=GetMessage("SEARCH_OPERATOR")?></td><td valign="top"><?=GetMessage("SEARCH_SYNONIM")?></td>
+                            <td><?=GetMessage("SEARCH_DESCRIPTION")?></td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top"><?=GetMessage("SEARCH_AND")?></td><td valign="top">and, &amp;, +</td>
+                            <td><?=GetMessage("SEARCH_AND_ALT")?></td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top"><?=GetMessage("SEARCH_OR")?></td><td valign="top">or, |</td>
+                            <td><?=GetMessage("SEARCH_OR_ALT")?></td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top"><?=GetMessage("SEARCH_NOT")?></td><td valign="top">not, ~</td>
+                            <td><?=GetMessage("SEARCH_NOT_ALT")?></td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top">( )</td>
+                            <td valign="top">&nbsp;</td>
+                            <td><?=GetMessage("SEARCH_BRACKETS_ALT")?></td>
+                        </tr>
+                    </table>
+                <?php } elseif (count($arResult["SEARCH"]) > 0) { ?>
+                    <?php if ($arParams["DISPLAY_TOP_PAGER"] != "N") { ?>
+                        <?= $arResult["NAV_STRING"]; ?>
                     <?php } ?>
-                </div>
+                    <div class="news-item">
+                        <?php foreach ($arResult["SEARCH"] as $arItem) { ?>
+                            <?php
+                                $dt = date_create_from_format('d.m.Y', $arItem['DATE_CHANGE']);
+                            ?>
+                        <div class="news-item__content">
+                            <div class="news-item__content--preview">
+                                <div class="preview__date"> <span class="preview__date--day"><?=$dt->format('d');?></span>
+                                    <p class="preview__date--month"><?=FormatDate('F', $dt->format('U'));?></p>
+                                </div>
+                                <a class="preview__more" href="<?echo $arItem["URL"]?>">
+                                    <div class="preview__more--btn">Подробее</div>
+                                    <div class="preview__more--arrow">
+                                        <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="news-item__content--text">
+                                <div class="text__title"><?= $arItem["TITLE_FORMATED"]?></div>
+                                <div class="text__prefix"><?= $arItem["BODY_FORMATED"]?></div>
+                                <a class="text__more" href="<?echo $arItem["URL"]?>">
+                                    <div class="text__more--btn">Подробее</div>
+                                    <div class="text__more--arrow">
+                                        <svg width="20" height="7" viewBox="0 0 20 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M1 1H19L13.6486 6" stroke="#424346" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <?php } ?>
+                    </div>
 
-                <?php if ($arParams["DISPLAY_BOTTOM_PAGER"] != "N") { ?>
-                    <?= $arResult["NAV_STRING"];?>
+                    <?php if ($arParams["DISPLAY_BOTTOM_PAGER"] != "N") { ?>
+                        <?= $arResult["NAV_STRING"];?>
+                    <?php } ?>
+
+                <?php } else { ?>
+                    <?ShowNote(GetMessage("SEARCH_NOTHING_TO_FOUND"));?>
                 <?php } ?>
-
-            <?php } else { ?>
-                <?ShowNote(GetMessage("SEARCH_NOTHING_TO_FOUND"));?>
-            <?php } ?>
-
+            </div>
         </div>
     </div>
 </section>
