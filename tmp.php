8379c8b2fc700f3c1983e097e587e3e064719c81
Sergey Fomin <sfomin@synergy.ru>
2022-11-19 13:29:08 +0300

    test curl

--- tmp.php
+++ tmp.php
@@ -8,10 +8,16 @@
  */
 
 require_once($_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php');
-require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+//require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+
+define("BODY_CLASS", "INDEX");
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $landerUrl = 'https://graviton.ru/local/ajax/controller.php?className=CheckCurl&action=tryRequest';
 
 $resource   = curl_init($landerUrl);
 
 \Mpakfm\Printu::obj($resource)->title('$resource')->response('html');
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
