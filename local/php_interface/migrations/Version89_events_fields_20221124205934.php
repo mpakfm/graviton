876a5645455570ce5b81350ccd046c5c01f2a279
Sergey Fomin <sfomin@synergy.ru>
2022-11-24 20:59:51 +0300

    89 migration

---
+++ local/php_interface/migrations/Version89_events_fields_20221124205934.php
@@ -0,0 +1,348 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version89_events_fields_20221124205934 extends Version
+{
+    protected $description = "";
+
+    protected $moduleVersion = "4.1.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $iblockId = $helper->Iblock()->getIblockIdIfExists('events', 'content');
+        $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Шапка. Дата',
+  'ACTIVE' => 'Y',
+  'SORT' => '100',
+  'CODE' => 'TOP_DATE',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Шапка. Название',
+  'ACTIVE' => 'Y',
+  'SORT' => '200',
+  'CODE' => 'TOP_TITLE',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Шапка. Город',
+  'ACTIVE' => 'Y',
+  'SORT' => '300',
+  'CODE' => 'TOP_PLACE',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Город',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'CITY',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'E',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'Y',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => 'references:city',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'Y',
+  'VERSION' => '2',
+  'USER_TYPE' => 'EAutocomplete',
+  'USER_TYPE_SETTINGS' => 
+  array (
+    'VIEW' => 'E',
+    'SHOW_ADD' => 'N',
+    'MAX_WIDTH' => 0,
+    'MIN_HEIGHT' => 24,
+    'MAX_HEIGHT' => 1000,
+    'BAN_SYM' => ',;',
+    'REP_SYM' => ' ',
+    'OTHER_REP_SYM' => '',
+    'IBLOCK_MESS' => 'N',
+  ),
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Подзаголовок',
+  'ACTIVE' => 'Y',
+  'SORT' => '1000',
+  'CODE' => 'DETAIL_SUB_TITLE',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '100',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Баннер',
+  'ACTIVE' => 'Y',
+  'SORT' => '1100',
+  'CODE' => 'DETAIL_BANNER',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'F',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Лозунг',
+  'ACTIVE' => 'Y',
+  'SORT' => '1200',
+  'CODE' => 'DETAIL_SLOGAN',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Адрес регистрации',
+  'ACTIVE' => 'Y',
+  'SORT' => '1300',
+  'CODE' => 'DETAIL_LINK',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Медиатека (фото события)',
+  'ACTIVE' => 'Y',
+  'SORT' => '1400',
+  'CODE' => 'DETAIL_PHOTO',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'F',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'Y',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->UserOptions()->saveElementForm($iblockId, array (
+  'Элемент|edit1' => 
+  array (
+    'ID' => 'ID',
+    'DATE_CREATE' => 'Создан',
+    'TIMESTAMP_X' => 'Изменен',
+    'ACTIVE' => 'Активность',
+    'ACTIVE_FROM' => 'Начало активности',
+    'ACTIVE_TO' => 'Окончание активности',
+    'NAME' => 'Название',
+    'CODE' => 'Символьный код',
+    'SORT' => 'Сортировка',
+    'IBLOCK_ELEMENT_PROP_VALUE' => 'Значения свойств',
+    'PROPERTY_IS_PUB' => 'Опубликовано',
+    'PROPERTY_IS_SLIDER' => 'Показывать в слайдере',
+    'PROPERTY_CITY' => 'Город',
+  ),
+  'Анонс|edit5' => 
+  array (
+    'PREVIEW_PICTURE' => 'Картинка для анонса',
+    'PREVIEW_TEXT' => 'Описание для анонса',
+  ),
+  'Подробно|edit6' => 
+  array (
+    'PROPERTY_TOP_TITLE' => 'Шапка. Название',
+    'PROPERTY_TOP_PLACE' => 'Шапка. Город',
+    'PROPERTY_TOP_DATE' => 'Шапка. Дата',
+    'DETAIL_PICTURE' => 'Детальная картинка',
+    'PROPERTY_DETAIL_SUB_TITLE' => 'Подзаголовок',
+    'DETAIL_TEXT' => 'Детальное описание',
+    'PROPERTY_DETAIL_BANNER' => 'Баннер',
+    'PROPERTY_DETAIL_SLOGAN' => 'Лозунг',
+    'PROPERTY_DETAIL_LINK' => 'Адрес регистрации',
+    'PROPERTY_DETAIL_PHOTO' => 'Медиатека (фото события)',
+  ),
+  'SEO|edit14' => 
+  array (
+    'IPROPERTY_TEMPLATES_ELEMENT_META_TITLE' => 'Шаблон META TITLE',
+    'IPROPERTY_TEMPLATES_ELEMENT_META_KEYWORDS' => 'Шаблон META KEYWORDS',
+    'IPROPERTY_TEMPLATES_ELEMENT_META_DESCRIPTION' => 'Шаблон META DESCRIPTION',
+    'IPROPERTY_TEMPLATES_ELEMENT_PAGE_TITLE' => 'Заголовок элемента',
+    'IPROPERTY_TEMPLATES_ELEMENTS_PREVIEW_PICTURE' => 'Настройки для картинок анонса элементов',
+    'IPROPERTY_TEMPLATES_ELEMENT_PREVIEW_PICTURE_FILE_ALT' => 'Шаблон ALT',
+    'IPROPERTY_TEMPLATES_ELEMENT_PREVIEW_PICTURE_FILE_TITLE' => 'Шаблон TITLE',
+    'IPROPERTY_TEMPLATES_ELEMENT_PREVIEW_PICTURE_FILE_NAME' => 'Шаблон имени файла',
+    'IPROPERTY_TEMPLATES_ELEMENTS_DETAIL_PICTURE' => 'Настройки для детальных картинок элементов',
+    'IPROPERTY_TEMPLATES_ELEMENT_DETAIL_PICTURE_FILE_ALT' => 'Шаблон ALT',
+    'IPROPERTY_TEMPLATES_ELEMENT_DETAIL_PICTURE_FILE_TITLE' => 'Шаблон TITLE',
+    'IPROPERTY_TEMPLATES_ELEMENT_DETAIL_PICTURE_FILE_NAME' => 'Шаблон имени файла',
+    'SEO_ADDITIONAL' => 'Дополнительно',
+    'TAGS' => 'Теги',
+  ),
+  'Разделы|edit2' => 
+  array (
+    'SECTIONS' => 'Разделы',
+  ),
+));
+    $helper->UserOptions()->saveElementGrid($iblockId, array (
+  'views' => 
+  array (
+    'default' => 
+    array (
+      'columns' => 
+      array (
+        0 => 'NAME',
+        1 => 'ACTIVE',
+        2 => 'SORT',
+        3 => 'ID',
+        4 => 'DATE_ACTIVE_FROM',
+        5 => 'PROPERTY_IS_PUB',
+        6 => 'PROPERTY_IS_SLIDER',
+        7 => 'PROPERTY_TOP_PLACE',
+        8 => 'PROPERTY_CITY',
+      ),
+      'columns_sizes' => 
+      array (
+        'expand' => 1,
+        'columns' => 
+        array (
+        ),
+      ),
+      'sticked_columns' => 
+      array (
+      ),
+      'last_sort_by' => 'date_active_from',
+      'last_sort_order' => 'desc',
+      'custom_names' => NULL,
+    ),
+  ),
+  'filters' => 
+  array (
+  ),
+  'current_view' => 'default',
+));
+
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
